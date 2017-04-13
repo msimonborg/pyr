a2dc627e1c7118b63229ccec79fc55dc5b093bfb
M. Simon Borg <msimonborg@gmail.com>
2017-04-13 12:59:45 -0400

    update reps resource object documentation

--- lib/pyr/resources/reps.rb
+++ lib/pyr/resources/reps.rb
@@ -4,25 +4,64 @@ module PYR
   class Resource
     # Request object for sending requests to the /reps resource of the API
     class Reps < Resource
+      # Read the params
       attr_reader :lat, :long, :address, :generate
 
-      def lat=(value)
-        @lat = Param.new(:lat, value)
+      # Set the lat param : float
+      # Needs to be accompanied by :long.
+      #
+      # Usage:
+      #   PYR.call :reps do |r|
+      #     r.lat  = 45.00000
+      #     r.long = -45.00000
+      #   end
+      def lat=(float)
+        @lat = Param.new(:lat, float)
         params << @lat
       end
 
-      def long=(value)
-        @long = Param.new(:long, value)
+      # Set the long param : float
+      # Needs to be accompanied by :lat.
+      #
+      # Usage:
+      #   PYR.call :reps do |r|
+      #     r.lat  = 45.0
+      #     r.long = -45.0
+      #   end
+      #
+      # Resulting URI: 'https://phone-your-rep.herokuapp.com/api/beta/reps?lat=45.0&long=-45.0'
+      def long=(float)
+        @long = Param.new(:long, float)
         params << @long
       end
 
-      def address=(value)
-        @address = Param.new(:address, value)
+      # Set the address param : string
+      # :lat and :long take precedence
+      #
+      # Usage:
+      #   PYR.call(:reps) { |r| r.address = '123 Main St., USA'}
+      #
+      # Resulting URI: 'https://phone-your-rep.herokuapp.com/api/beta/reps?lat=45.0&long=-45.0'
+      def address=(string)
+        @address = Param.new(:address, string)
         params << @address
       end
 
-      def generate=(value)
-        @generate = Param.new(:generate, value)
+      # Set the generate param : boolean
+      #
+      # By default making a call to the full :reps index will return a cached
+      # JSON file. The cached file is updated regularly (usually every day).
+      # However, if you want to generate a fresh response directly from the
+      # database, you can set :generate to true. The response will be much
+      # slower, and in most cases will never be necessary. Has no effect
+      # when used with either :address or :lat/:long
+      #
+      # Usage:
+      #   PYR.call(:reps) { |r| r.generate = true }
+      #
+      # Resulting URI: 'https://phone-your-rep.herokuapp.com/api/beta/reps?generate=true'
+      def generate=(boolean)
+        @generate = Param.new(:generate, boolean)
         params << @generate
       end
     end
